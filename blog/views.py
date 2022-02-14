3bc6c6e596b0083e155d8a3ec6004c4c3371e157
yusun <yousun2286@naver.com>
2022-02-14 11:35:13 +0900

    view.py

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 # Create your views here.
-# Views 내에 선언된 함수 -> 인자로 HttpRequest라는 객체를 Django가 전달
+# Views 내에 선언된 함수 -> 인자로 HttpRequest 라는 객체를 Django 가 전달
+
+
 def post_list(request):
     my_name = '장고웹프레임워크'
     http_method = request.method
-    return HttpResponse('''
-        <h2> Welcome {name}</h2>
-        <p> Http Method : {method} </p>
-        <p> Http Encoding : {encod} </p>
-        <p> Http Path : {my_path} </p>
-    '''.format(name=my_name, method=http_method, encod=request.encoding, my_path=request.path))
+    # return HttpResponse('''
+    #     <h2> Welcome {name}</h2>
+    #     <p> Http Method : {method} </p>
+    #     <p> Http headers User-Agent : {header} </p>
+    #     <p> Http Path : {my_path} </p>
+    # '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], my_path=request.path))
+    # return render(request, 'blog/post_list.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'post_list': posts})
--- mydjango/settings.py
+++ mydjango/settings.py
@@ -112,6 +112,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
