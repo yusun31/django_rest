18ec5c20fe1c47c75199f9c932392adf0fb9b7d5
yusun <yousun2286@naver.com>
2022-02-15 13:43:36 +0900

    urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # http://localhost:8000/blog
     path('', views.post_list, name='post_list_home'),
+    # http://localhost:8000/blog/post/5
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    # http://localhost:8000/blog/post/new
+    path('post/new', views.post_new, name='post_new'),
 ]
