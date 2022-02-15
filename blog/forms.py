998e0306a1bea3e356807f6f6d300694b0852223
yusun <yousun2286@naver.com>
2022-02-15 14:40:59 +0900

    forms

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,15 @@ class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text', )
+
+
+def min_length_5_validator(value):
+    if len(value) < 5:
+        # ValidatorError 예외 강제로 발생시킴
+        raise forms.ValidationError('text는 5글자 이상 입력해주세요!')
+
+
+# Form을 상속받는 PostForm 클래스 정의
+class PostForm(forms.Form):
+    title = forms.CharField()
+    text = forms.CharField(widget=forms.Textarea, validators=[min_length_5_validator])
