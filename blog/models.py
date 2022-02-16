ee8f12401b6daf81cfa24bc10d2183e6197e7663
yusun <yousun2286@naver.com>
2022-02-16 16:13:04 +0900

    models

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,29 @@ class Post(models.Model):
     # Model 클래스에 정의된 __str__함수를 재정의(title 필드값을 리턴)
     def __str__(self):
         return self.title + '(' + str(self.id) + ')'
+
+    # Post가 참조하는 Comment 중에서 승인된 Comment만 필터링하기
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+# 댓글(Comment) Model 클래스 선언
+class Comment(models.Model):
+    # 참조하는 Post 객체
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    # 작성자
+    author = models.CharField(max_length=200)
+    # 내용
+    text = models.TextField()
+    # 작성일
+    created_date = models.DateTimeField(default=timezone.now)
+    # 승인여부
+    approved_comment = models.BooleanField(default=False)
+
+    # 댓글 승인
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text + '(' + str(self.id) + ')'
