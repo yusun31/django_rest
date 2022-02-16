cd16034bcefcfe1bf23120c34edddba490413a39
yusun <yousun2286@naver.com>
2022-02-16 16:13:27 +0900

    views

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,70 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .models import Post
-from .forms import PostModelForm, PostForm
+from .models import Post, Comment
+from .forms import PostModelForm, PostForm, CommentForm
 
 
+# 댓글 승인
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+# 댓글 삭제
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+
+# 댓글 등록
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+# 글 삭제
+def post_remove(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('post_list_home')
+
+
+@login_required
+# 글 수정(ModelForm 사용)
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'postform': form})
+
+
+@login_required
 # 글 등록(Form 사용)
 def post_new(request):
     if request.method == "POST":
