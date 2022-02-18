ebe17ca4a0412441f3a0f8e2f2f70e1dd9615569
yusun <yousun2286@naver.com>
2022-02-18 17:58:21 +0900

    views

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,40 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK
+
 from .models import Post, Comment
 from .forms import PostModelForm, PostForm, CommentForm
+
+
+# Token 발행하는 로그인
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get('username')
+    # email = request.data.get('email')
+    password = request.data.get('password')
+
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+
+    # 여기서 authenticate로 유저 validate
+    user = authenticate(username=username, password=password)
+    print('>>>> user ', user)
+
+    if not user:
+        return Response({'error': 'Invalid credentials'}, status=HTTP_404_NOT_FOUND)
+
+    # user 로 토큰 발행
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({'token': token.key}, status=HTTP_200_OK)
 
 
 # 글 목록 Pagination
